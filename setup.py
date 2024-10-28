68f61e6e732ebcc89dbef15ee9d5f8f647e2ca1e
Ivo Branco <ivo.branco@fccn.pt>
2024-10-28 15:37:10 +0000

    feat: add K8s HPA and resource cpu/memory constraints

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 def load_readme():
-    with io.open(os.path.join(HERE, "README.md"), "md", encoding="utf8") as f:
+    with io.open(os.path.join(HERE, "README.md"), "rt", encoding="utf8") as f:
         return f.read()
 
 
@@ -38,7 +38,7 @@ setup(
     author_email="ivo.branco@fccn.pt",
     description="Installs the nau-course-certificate project that allows to ser",
     long_description=load_readme(),
-    long_description_content_type="text/x-rst",
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.8",
